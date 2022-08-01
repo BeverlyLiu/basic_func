b60a347ce5fc2e1a48e81ae114a85c7ed58fe55c
BeverlyLiu <bevliu@gmail.com>
2022-08-01 12:19:12 -0700

    added converter

--- if_else.py
+++ if_else.py
@@ -9,4 +9,13 @@ elif temperature > 10: #(10, 20]
     print("It's a cold day")
 else:
     print("It's cold")
+
+weight = int(input("Weight: "))
+unit = input("(K)g or (L)bs: ")
+if unit.upper() == "K":
+    converted = weight / 0.45
+    print("Weight in Lbs: " + str(converted))
+else:
+    converted = weight * 0.45
+    print("Weight in Kgs:" + str(converted))
 print("Done")

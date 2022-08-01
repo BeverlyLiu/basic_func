a35e61c2fd543052cf8e7ef4bdf4139d2c95ef73
BeverlyLiu <bevliu@gmail.com>
2022-08-01 13:21:38 -0700

    while loops

---
+++ while_loops.py
@@ -0,0 +1,12 @@
+i = 1
+while i <= 5:
+    print(i * ' *')
+    i += 1
+
+nums = [1, 2, 3, 4, 5]
+j = 0
+while j < len(nums):
+    print(i * '* ' + str(nums[j]))
+    i -= 1
+    j += 1
+print("Done")

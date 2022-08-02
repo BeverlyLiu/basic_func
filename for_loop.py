854fe481df237d8696fbea8778f88a82bce0701c
BeverlyLiu <bevliu@gmail.com>
2022-08-02 12:37:44 -0700

    for loops

---
+++ for_loop.py
@@ -0,0 +1,24 @@
+nums = [1, 2, 3, 4, 5]
+item: int
+
+for item in nums:
+    print(item)
+
+#numbers = range(5)
+#numbers = range(5, 10)
+numbers = range(5, 10, 2)
+
+for number in numbers:
+    print(number)
+
+names = ["John", "Tom", "Mike", "Amy", "Susana"]
+#names.append("Sam")
+#names.insert(0, "Sam")
+#names.remove("Amy")
+#names.clear()
+names.reverse()
+
+for name in names:
+    print(name)
+
+print("Done")
